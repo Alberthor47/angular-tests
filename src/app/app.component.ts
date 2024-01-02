84cb8a4469b8dc1681c20bfb02c613194ad1c0f7
Alberto Camarena <alberto@camarena.me>
2024-01-02 13:08:47 -0600

    Last test to preview deploy in single branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,9 @@ export class AppComponent {
   title = 'todo-app';
   welcome = 'Bienvenido a mi primera aplicación con Angular';
   tasks = [
-    'Instalar Angular CLI',
-    'Crear proyecto',
-    'Crear componente',
-    'Crear servicio',
-    'Verificar si se actualiza con cada actualizacion al PR'
+    'Al parecer si seactualiza',
+    'Verificar para multiples ramas',
+    'Borrar al hacer merge del PR',
+    'Testear produccion final'
   ];
 }
