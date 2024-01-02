1f77deee84d801a19c03390162b162a19c4698c0
Alberto Camarena <alberto@camarena.me>
2024-01-02 13:23:10 -0600

    Hotfix to deploy prod

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,6 @@ export class AppComponent {
   title = 'todo-app';
   welcome = 'Bienvenido a mi primera aplicación con Angular';
   tasks = [
-    'Al parecer si seactualiza',
     'Verificar para multiples ramas',
     'Borrar al hacer merge del PR',
     'Testear produccion final'
